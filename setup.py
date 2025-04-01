da330bc3cec4b7843e7a4bc2ec16359ec3dd6ad9
Anupam Bhakta <ab5494@columbia.edu>
2025-04-01 19:30:00 -0400

    updated python version to 3.9 as minimum

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ setup(
         "numpy",
         "gym>=0.17.0",
         "pygame>=2.0.0",
+        "shimmy",
         "stable-baselines3[extra]",
         "torch"
     ],
-    author="Anupam Bhakta, Kevin Qiu",
-    author_email="ab5494@columbia.edu, kzq2000@columbia.edu",
+    author="Anupam Bhakta, Ben Fu, Kevin Qiu",
+    author_email="ab5494@columbia.edu, bhf2117@columbia.edu, kzq2000@columbia.edu",
     description="A reinforcement learning environment for cooperative transport",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -22,5 +23,5 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.9",
 )
